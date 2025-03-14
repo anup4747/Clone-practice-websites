64317f914fdc5cf386ad73c5bd3213cc3e00efbf
Anup <119606225+anup4747@users.noreply.github.com>
2025-03-14 10:45:52 +0530

    Recreating the vite project due to some error

---
+++ websiteOne/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import './index.css'
+import Navbar from './navbar'
+function App() {
+  
+  return (
+    <>
+    <Navbar/>
+      <h1 className="text-3xl font-bold underline">hello world</h1>
+    </>
+  )
+}
+
+export default App
