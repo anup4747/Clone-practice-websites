704391f8b59ffec185f0d2e607fb0951b2c05672
anupw <anupdtarwade@gmail.com>
2025-06-04 16:13:04 +0530

    adding tools in menu in topmenu

--- websiteTwo/excalidraw_clone/src/componants/toolsMenu.tsx
+++ websiteTwo/excalidraw_clone/src/componants/toolsMenu.tsx
@@ -8,7 +8,7 @@ import {
   BiLockOpen,
   BiLock,
   BiText,
-  BiCircle,     
+  BiCircle,
   BiNavigation,
   BiPhotoAlbum,
   BiEraser,
@@ -16,28 +16,38 @@ import {
   BiSquareRounded,
 } from "react-icons/bi";
 import { BsDiamond } from "react-icons/bs";
+import { FaPaintBrush } from "react-icons/fa";
+import { MdOutlineStarBorder } from "react-icons/md";
+import { GiPaintBucket } from "react-icons/gi";
 
 export const ToolsMenu: React.FC = () => {
-
-  const [locked,setLocked]  = useState(false);
-  const [hand,setHand] = useState(false);
-  const [selection,setSelection] = useState(false);
-  const [square,setSquare] = useState(false);
-  const [diamond,setDiamond] = useState(false);
-  const [circle,setCircle] = useState(false);
-  const [arrow,setArrow] = useState(false);
-  const [line,setLine] = useState(false);
+  const [locked, setLocked] = useState(false);
+  const [hand, setHand] = useState(false);
+  const [selection, setSelection] = useState(false);
+  const [square, setSquare] = useState(false);
+  const [diamond, setDiamond] = useState(false);
+  const [circle, setCircle] = useState(false);
+  const [arrow, setArrow] = useState(false);
+  const [line, setLine] = useState(false);
   const [pencil, setPencil] = useState(false);
-  const [text,setText] = useState(false);
-  const [image,setImage] = useState(false);
-  const [eraser,setEraser] = useState(false);
-  const [menu,setMenu] = useState(false);
+  const [text, setText] = useState(false);
+  const [image, setImage] = useState(false);
+  const [eraser, setEraser] = useState(false);
+  const [menu, setMenu] = useState(false);
+  const [star, setStar] = useState(false);
+  const [paintBucket, setPaintBucket] = useState(false);
+  const [paintBrush, setPaintBrush] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleLockToggle = () => {
-    setLocked(!locked)
-  }
+    setLocked(!locked);
+  };
 
-   const resetAllExcept = (activeSetter: (value: boolean) => void) => {
+  const handleToolsMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const resetAllExcept = (activeSetter: (value: boolean) => void) => {
     setLocked(false);
     setHand(false);
     setSelection(false);
@@ -52,6 +62,7 @@ export const ToolsMenu: React.FC = () => {
     setEraser(false);
     setMenu(false);
     activeSetter(true);
+    setIsMenuOpen(false);
   };
 
   // Keyboard event handler for number keys 1-9, 0
@@ -89,7 +100,7 @@ export const ToolsMenu: React.FC = () => {
         case "0":
           resetAllExcept(setEraser);
           break;
-        default: 
+        default:
           break;
       }
     };
@@ -100,66 +111,158 @@ export const ToolsMenu: React.FC = () => {
     };
   }, [locked]);
 
-
   return (
     <section className="p-2 bg-gray-800 rounded-xl">
       <div className="flex flex-row  justify-center items-center space-x-1">
-        <div onClick={handleLockToggle } className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${locked ? 'bg-blue-900 hover:bg-blue-900' : ''} `}>
-          {locked ?  <BiLock /> :  <BiLockOpen />}
+        <div
+          onClick={handleLockToggle}
+          className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${
+            locked ? "bg-blue-900 hover:bg-blue-900" : ""
+          } `}
+        >
+          {locked ? <BiLock /> : <BiLockOpen />}
         </div>
-        <div onClick={() => resetAllExcept(setHand)} className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${hand ? 'bg-blue-900 hover:bg-blue-900' : ''} `}>
+        <div
+          onClick={() => resetAllExcept(setHand)}
+          className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${
+            hand ? "bg-blue-900 hover:bg-blue-900" : ""
+          } `}
+        >
           {" "}
           <HiOutlineHand />
         </div>
-        <div onClick={() => resetAllExcept(setSelection)} className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${selection ? 'bg-blue-900 hover:bg-blue-900' : ''}`}>
+        <div
+          onClick={() => resetAllExcept(setSelection)}
+          className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${
+            selection ? "bg-blue-900 hover:bg-blue-900" : ""
+          }`}
+        >
           <BiNavigation />
         </div>
 
-        <div onClick={() => resetAllExcept(setSquare)} className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${square ? 'bg-blue-900 hover:bg-blue-900' : ''}`}>
+        <div
+          onClick={() => resetAllExcept(setSquare)}
+          className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${
+            square ? "bg-blue-900 hover:bg-blue-900" : ""
+          }`}
+        >
           <BiSquareRounded />
         </div>
-        <div onClick={() => resetAllExcept(setDiamond)} className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${diamond ? 'bg-blue-900 hover:bg-blue-900' : ''}`}>
+        <div
+          onClick={() => resetAllExcept(setDiamond)}
+          className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${
+            diamond ? "bg-blue-900 hover:bg-blue-900" : ""
+          }`}
+        >
           <BsDiamond />
         </div>
 
-        <div onClick={() => resetAllExcept(setCircle)} className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${circle ? 'bg-blue-900 hover:bg-blue-900' : ''}`}>
+        <div
+          onClick={() => resetAllExcept(setCircle)}
+          className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${
+            circle ? "bg-blue-900 hover:bg-blue-900" : ""
+          }`}
+        >
           {" "}
           <BiCircle />
         </div>
 
-        <div onClick={() => resetAllExcept(setArrow)} className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${arrow ? 'bg-blue-900 hover:bg-blue-900' : ''}`}>
+        <div
+          onClick={() => resetAllExcept(setArrow)}
+          className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${
+            arrow ? "bg-blue-900 hover:bg-blue-900" : ""
+          }`}
+        >
           {" "}
           <FiArrowRight />
         </div>
 
-        <div onClick={() => resetAllExcept(setLine)} className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${line ? 'bg-blue-900 hover:bg-blue-900' : ''}`}>
+        <div
+          onClick={() => resetAllExcept(setLine)}
+          className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${
+            line ? "bg-blue-900 hover:bg-blue-900" : ""
+          }`}
+        >
           {" "}
           <IoMdRemove />
         </div>
 
-        <div onClick={() => resetAllExcept(setPencil)} className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${pencil ? 'bg-blue-900 hover:bg-blue-900' : ''}`}>
+        <div
+          onClick={() => resetAllExcept(setPencil)}
+          className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${
+            pencil ? "bg-blue-900 hover:bg-blue-900" : ""
+          }`}
+        >
           {" "}
           <SlPencil />
         </div>
 
-        <div onClick={() => resetAllExcept(setText)} className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${text ? 'bg-blue-900 hover:bg-blue-900' : ''}`}>
+        <div
+          onClick={() => resetAllExcept(setText)}
+          className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${
+            text ? "bg-blue-900 hover:bg-blue-900" : ""
+          }`}
+        >
           <BiText />{" "}
         </div>
 
-        <div onClick={() => resetAllExcept(setImage)}  className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${image ? 'bg-blue-900 hover:bg-blue-900' : ''}`}>
+        <div
+          onClick={() => resetAllExcept(setImage)}
+          className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${
+            image ? "bg-blue-900 hover:bg-blue-900" : ""
+          }`}
+        >
           {" "}
           <BiPhotoAlbum />
         </div>
 
-        <div onClick={() => resetAllExcept(setEraser)}  className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${eraser ? 'bg-blue-900 hover:bg-blue-900' : ''}`}>
+        <div
+          onClick={() => resetAllExcept(setEraser)}
+          className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${
+            eraser ? "bg-blue-900 hover:bg-blue-900" : ""
+          }`}
+        >
           {" "}
           <BiEraser />
         </div>
 
-        <div onClick={() => resetAllExcept(setMenu)} className="cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm">
+        <div
+          onClick={handleToolsMenu}
+          className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${
+            isMenuOpen ? "bg-blue-900 hover:bg-blue-900" : ""
+          }`}
+        >
           <BiSolidGrid />
         </div>
       </div>
+      {isMenuOpen && (
+        <div className="absolute top-12 right-2 bg-gray-800 rounded-xl p-2 flex flex-col gap-2 shadow-lg z-10">
+          <div
+            onClick={() => resetAllExcept(setPaintBrush)}
+            className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${
+              paintBrush ? "bg-blue-500" : ""
+            }`}
+          >
+            <FaPaintBrush />
+          </div>
+          <div
+            onClick={() => resetAllExcept(setStar)}
+            className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${
+              star ? "bg-blue-500" : ""
+            }`}
+          >
+            <MdOutlineStarBorder />
+          </div>
+          <div
+            onClick={() => resetAllExcept(setPaintBucket)}
+            className={`cursor-pointer text-white text-2xl p-1 hover:bg-gray-700 rounded-sm ${
+              paintBucket ? "bg-blue-500" : ""
+            }`}
+          >
+            <GiPaintBucket />
+          </div>
+        </div>
+      )}
     </section>
   );
 };
